cd4c564b32cde5fb7e7deb783531d3d6c50a25b4
Ali Reza Farahnak <52029052+alfarahn@users.noreply.github.com>
2024-12-06 12:23:05 +0200

    Add files via upload

---
+++ Utilities/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Utilities;
+
+public static class Constants
+{
+    public const string Gpt4omini = "gpt-4o-mini";
+    public const string Gpt4o = "gpt-4o";
+    public const string Dalle3 = "dalle3";
+    public const string Endpoint = "https://developer-productivity-ai.openai.azure.com";
+    public const string ApiKey = "";
+    public const string GitHubPAT = "";
+}
